0474ef00654a5f0daa0c484c7030ef6f5e2331e0
manuelgaleone <galeonemanuel14@gmail.com>
2022-11-17 00:39:12 +0100

    Fixing textelements display none

--- src/components/SingleMovie.vue
+++ src/components/SingleMovie.vue
@@ -16,7 +16,8 @@ export default {
             '../assets/img/ita.svg',
             '../assets/img/usa.svg',
             '../assets/img/vatican.svg'
-            ]
+            ],
+            hover: false
         }
     },
     methods: {
@@ -111,8 +112,8 @@ export default {
                 <img v-if="movie.poster_path === null || movie.poster_path === undefined" class="movie_image p-3" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/00d53663-c0cd-46e9-9aba-bed9180648cd/d5c3vm5-20410fd2-bb67-4a27-870e-adb06437a71c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAwZDUzNjYzLWMwY2QtNDZlOS05YWJhLWJlZDkxODA2NDhjZFwvZDVjM3ZtNS0yMDQxMGZkMi1iYjY3LTRhMjctODcwZS1hZGIwNjQzN2E3MWMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BADD_GnMLXva7O9jwAHBzsKBhvpI_oAyjiccAPItofE" alt="Movie Image">
                 <img v-else class="movie_image p-3" :src="data.movieImage + movie.poster_path" alt="Movie Image">
             </div> -->
-            <div v-if="movie.poster_path === null || movie.poster_path === undefined" :style="{backgroundImage:'url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/00d53663-c0cd-46e9-9aba-bed9180648cd/d5c3vm5-20410fd2-bb67-4a27-870e-adb06437a71c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAwZDUzNjYzLWMwY2QtNDZlOS05YWJhLWJlZDkxODA2NDhjZFwvZDVjM3ZtNS0yMDQxMGZkMi1iYjY3LTRhMjctODcwZS1hZGIwNjQzN2E3MWMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BADD_GnMLXva7O9jwAHBzsKBhvpI_oAyjiccAPItofE)'}"  class="movie_elements movie_card text-left">
-                <div class="text_elements py-5 d_flex column">
+            <div v-if="movie.poster_path === null || movie.poster_path === undefined" :style="{backgroundImage:'url(https://bit.ly/3V4vcOs)'}" class="movie_elements movie_card text-left">
+                <div @mouseover="hover = true" @mouseleave="hover = false" :class="{ active: hover }"    class="text_elements py-5 d_flex column">
                     <h3 class="movie_title">
                         {{movie.title}}
                         {{movie.name}}
