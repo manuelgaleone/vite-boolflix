0bdcb5c4f303376b4b5350e2a274292902c066b0
manuelgaleone <galeonemanuel14@gmail.com>
2022-11-16 17:10:03 +0100

    Fixing img null

--- src/components/SingleMovie.vue
+++ src/components/SingleMovie.vue
@@ -28,7 +28,8 @@ export default {
 <template>
     <div class="col-3 px-2 py-2 ">
         <div class="movie movie_card p-2 text-center">
-            <img class="movie_image p-3" :src="data.movieImage + movie.poster_path" alt="Movie Image">
+            <img v-if="movie.poster_path === null" class="movie_image p-3" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/00d53663-c0cd-46e9-9aba-bed9180648cd/d5c3vm5-20410fd2-bb67-4a27-870e-adb06437a71c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAwZDUzNjYzLWMwY2QtNDZlOS05YWJhLWJlZDkxODA2NDhjZFwvZDVjM3ZtNS0yMDQxMGZkMi1iYjY3LTRhMjctODcwZS1hZGIwNjQzN2E3MWMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BADD_GnMLXva7O9jwAHBzsKBhvpI_oAyjiccAPItofE" alt="Movie Image">
+            <img v-else class="movie_image p-3" :src="data.movieImage + movie.poster_path" alt="Movie Image">
             <h3 class="movie_title p-2">
                 {{movie.title}}
                 {{movie.name}}
