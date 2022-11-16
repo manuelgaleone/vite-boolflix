312ae581c948c896019ae08be382b7e5783a1336
manuelgaleone <galeonemanuel14@gmail.com>
2022-11-16 12:07:59 +0100

    Initial commit - Importing contents and install

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import MainContent from "./components/MainContent.vue";
+
+export default {
+  name:"App",
+  components: {
+    MainContent
+  }
+}
+</script>
+
+<template>
+  <MainContent></MainContent>
+</template>
+
+<style>
+</style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "MainContent",
+    components: {
+
+    }
+}
+</script>
+
+<template>
+    <h1 class="color_white">
+        Ciao!
+    </h1>
+</template>
