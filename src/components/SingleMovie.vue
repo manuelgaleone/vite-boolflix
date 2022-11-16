c94c51759ecf6b26d6dd48d01d1d469e8872503c
manuelgaleone <galeonemanuel14@gmail.com>
2022-11-16 17:37:18 +0100

    Add the stars function

--- src/components/SingleMovie.vue
+++ src/components/SingleMovie.vue
@@ -27,26 +27,55 @@ export default {
 
 <template>
     <div class="col-3 px-2 py-2 ">
-        <div class="movie movie_card p-2 text-center">
-            <img v-if="movie.poster_path === null" class="movie_image p-3" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/00d53663-c0cd-46e9-9aba-bed9180648cd/d5c3vm5-20410fd2-bb67-4a27-870e-adb06437a71c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAwZDUzNjYzLWMwY2QtNDZlOS05YWJhLWJlZDkxODA2NDhjZFwvZDVjM3ZtNS0yMDQxMGZkMi1iYjY3LTRhMjctODcwZS1hZGIwNjQzN2E3MWMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BADD_GnMLXva7O9jwAHBzsKBhvpI_oAyjiccAPItofE" alt="Movie Image">
-            <img v-else class="movie_image p-3" :src="data.movieImage + movie.poster_path" alt="Movie Image">
-            <h3 class="movie_title p-2">
-                {{movie.title}}
-                {{movie.name}}
-            </h3>
-            <p class="movie_media p-2">
-                {{movie.media_type}}
-            </p>
-            <h4 class="movie_original_title">
-                {{movie.original_title}}
-                {{movie.original_name}}
-            </h4>
-            <div class="language">
-                <p class="no_flag" >Bandiera non presente.</p>
+        <div class="movie movie_card p-2 text_center">
+            <div class="movie_imgs">
+                <img v-if="movie.poster_path === null" class="movie_image p-3" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/00d53663-c0cd-46e9-9aba-bed9180648cd/d5c3vm5-20410fd2-bb67-4a27-870e-adb06437a71c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAwZDUzNjYzLWMwY2QtNDZlOS05YWJhLWJlZDkxODA2NDhjZFwvZDVjM3ZtNS0yMDQxMGZkMi1iYjY3LTRhMjctODcwZS1hZGIwNjQzN2E3MWMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BADD_GnMLXva7O9jwAHBzsKBhvpI_oAyjiccAPItofE" alt="Movie Image">
+                <img v-else class="movie_image p-3" :src="data.movieImage + movie.poster_path" alt="Movie Image">
             </div>
-            <p class="vote py-3">
-                {{movie.vote_average}}
-            </p>
+            <div class="movie_elements text_center">
+                <h3 class="movie_title p-2">
+                    {{movie.title}}
+                    {{movie.name}}
+                </h3>
+                <p class="movie_media p-2">
+                    {{movie.media_type}}
+                </p>
+                <h4 class="movie_original_title">
+                    {{movie.original_title}}
+                    {{movie.original_name}}
+                </h4>
+                <div class="language">
+                    <p class="no_flag" >Bandiera non presente.</p>
+                </div>
+                <div v-if="(Math.ceil(movie.vote_average)) === 0" class="vote py-3 d_flex align_items justify_center">
+                    0 voti.
+                </div>
+                <div v-if="(Math.ceil(movie.vote_average)) > 1 < 2" class="vote py-3 d_flex align_items justify_center p-1">
+                    <i class="fa-solid fa-star star_review"></i>
+                </div>
+                <div v-if="(Math.ceil(movie.vote_average)) > 2 < 4" class="vote py-3 d_flex align_items justify_center p-1">
+                    <i class="fa-solid fa-star star_review"></i>
+                    <i class="fa-solid fa-star star_review"></i>
+                </div>
+                <div v-if="(Math.ceil(movie.vote_average)) > 4 < 6" class="vote py-3 d_flex align_items justify_center p-1">
+                    <i class="fa-solid fa-star star_review"></i>
+                    <i class="fa-solid fa-star star_review"></i>
+                    <i class="fa-solid fa-star star_review"></i>
+                </div>
+                <div v-if="(Math.ceil(movie.vote_average)) > 6 < 8" class="vote py-3 d_flex align_items justify_center p-1">
+                    <i class="fa-solid fa-star star_review"></i>
+                    <i class="fa-solid fa-star star_review"></i>
+                    <i class="fa-solid fa-star star_review"></i>
+                    <i class="fa-solid fa-star star_review"></i>
+                </div>
+                <div v-if="(Math.ceil(movie.vote_average)) > 8 < 10" class="vote py-3 d_flex align_items justify_center p-1">
+                    <i class="fa-solid fa-star star_review"></i>
+                    <i class="fa-solid fa-star star_review"></i>
+                    <i class="fa-solid fa-star star_review"></i>
+                    <i class="fa-solid fa-star star_review"></i>
+                    <i class="fa-solid fa-star star_review"></i>
+                </div>
+            </div>
         </div>
     </div>
 </template>
